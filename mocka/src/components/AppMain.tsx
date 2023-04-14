cb80cb4236f5aca27ee7e3fed30e39ad4fdecf4b
Mytchall Bransgrove <mytchallbransgrove@hayleys-macbook-pro.hairylemon.local>
2023-04-14 13:09:03 +1200

    got react component added

---
+++ mocka/src/components/AppMain.tsx
@@ -0,0 +1,33 @@
+const AppMain = (): JSX.Element => {
+    return (
+    <>
+    <div className="bg-white p-2 flex">
+    <div className="bg-gray-200 p-2 rounded-lg w-[60vw]">
+        <div id="content" className="">
+            <div id="sidebar">
+                <div id="nav" className="flex justify-between">
+                    <p className="font-bold text-2xl">Mocka</p>
+                    <button>Toggle</button>
+                </div>
+                <div id="searchbar">
+
+                </div>
+                <div id="component-list">
+
+                </div>
+
+            </div>
+            <div id="components">
+
+            </div>
+        </div>
+        
+    </div>
+    <div className="w-[40vw] bg-white">
+
+        </div>
+    </div>
+    </>);
+}
+
+export default AppMain;
